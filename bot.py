170fafd216d22b28639a4701e6a3b4d59839d46c
2frikinbeast <53154447+2frikinbeast@users.noreply.github.com>
2020-08-30 21:39:00 -0400

    Initial commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+
+TOKEN = 'NTMyMzI2MzQzNzUzNTk2OTM4.XDUlig.iCfIuLNLpHBfBtAE3DnHeDFQuAw'
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    discord_input = message.content
+    print(discord_input)
+
+    if discord_input.lower() == "!hello":
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send(msg)
+
+    if "cringe" in discord_input.lower() or "based" in discord_input.lower():
+        msg = "\"Hurr, Cringe! Durr, Cringe! Cringe!\" \nIs that all you shitposting fucks can say?! \n\"ugh, based based based cringe cringe cringe based based cringe cringe\" \nI feel like I'm in a fucking asylum full of dementering old people, that can do nothing but repeat the same fucking words and look like a fucking broken record! \n\"Cringe cringe cringe cringe! cringe based based! onions onions soy! onions LOL onions! Cringe boomer Le zoomer! I am boomer?! No zoom zoom zoomies, zoomer going zoomies!\" \nAGH I FUCKING HATE THE INTERNET SO GOD DAMN MUCH!! FUCK SHITPOSTING HONEST GOD-FUCKING I HOPE YOUR MOTHER CHOKES ON HER OWN FECES IN HELL! YOU COCKSUCKER! \nOH BUT I KNOW MY POST IS CRINGED ISN'T IT? CRINGE CRINGE CRINGEY-CRINGE BASED CRINGE REDDIT CRINGE BASED REDDIT ONIONS BASED ONIONS CRINGE REDDIT CRINGE"
+        await message.channel.send(msg)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+client.run(TOKEN)
