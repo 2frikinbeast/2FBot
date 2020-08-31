c7e061016a29eae39b6131bead464715fadb36cc
2frikinbeast <53154447+2frikinbeast@users.noreply.github.com>
2020-08-31 00:31:22 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-import discord
 import os
+import discord
 
 TOKEN = os.environ.get('2FBOT_TOKEN')
 
@@ -12,20 +12,46 @@ cringe_copypasta = ["\"Hurr, Cringe! Durr, Cringe! Cringe!\"",
                     "\"ugh, based based based cringe cringe cringe based based cringe cringe\"",
                     "I feel like I'm in a fucking asylum full of dementering old people, that can do nothing but "
                     "repeat the same fucking words and look like a fucking broken record!",
-                    "\"Cringe cringe cringe cringe! cringe based based! onions onions soy! onions LOL onions! Cringe "
+                    "\"Cringe cringe cringe cringe! cringe based based! onions onions snoy! onions LOL onions! Cringe "
                     "boomer Le zoomer! I am boomer?! No zoom zoom zoomies, zoomer going zoomies!\"",
                     "AGH I FUCKING HATE THE INTERNET SO GOD DAMN MUCH!! FUCK SHITPOSTING HONEST GOD-FUCKING I HOPE "
                     "YOUR MOTHER CHOKES ON HER OWN FECES IN HELL! YOU COCKSUCKER!",
                     "OH BUT I KNOW MY POST IS CRINGED ISN'T IT? CRINGE CRINGE CRINGEY-CRINGE BASED CRINGE REDDIT "
                     "CRINGE BASED REDDIT ONIONS BASED ONIONS CRINGE REDDIT CRINGE"]
 command_list = ["help", "hello"]
+stand_list = []
 
 
-def list_to_linebroken_string(list):
+def list_to_linebroken_string(string_list):
     output = ""
-    for item in list:
+    for item in string_list:
         output += (item + "\n")
     return output
+
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+
+def remove_suffix(text, suffix):
+    if text.endswith(suffix):
+        return text[:(-1 * len(suffix))]
+    return text
+
+
+def stand_stats(param):
+    return
+
+
+with open('stand_list.txt') as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+        stand_list.append(remove_suffix(line, '\n'))
+        line = fp.readline()
+        cnt += 1
 
 
 @client.event
@@ -43,6 +69,10 @@ async def on_message(message):
     if discord_input.lower() == (command_prefix + "hello"):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
+
+    if discord_input.lower().startswith(command_prefix + "stand"):
+        stand_query = remove_prefix(discord_input, (command_prefix + "stand "))
+        await message.channel.send(stand_stats(stand_query))
 
     for i in cringe_words:
         if i in discord_input.lower():
